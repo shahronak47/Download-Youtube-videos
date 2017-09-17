ecf6dd5ea3ee2f6e65b149138699eb98f26e723e
Ronak Shah <shahronak47@yahoo.in>
2017-09-17 23:40:27 +0800

    get all the videos from a channel

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import yaml
 from googleapiclient.discovery import build
 
 #Autheniticating the developer keys
-envconfig = 'C:\Users\Ronak Shah\Google Drive\Download Youtube videos/config_YT.yml'
+envconfig = 'C:\Users\Ronak Shah\Google Drive\Documents/config_YT.yml'
 with open(envconfig, 'r') as f:
     doc = yaml.load(f)
 
@@ -27,8 +27,6 @@ def youtube_search_by_term(service, keyword):
             url_lists.append(item['id']['videoId'])
         except :
             pass
-
-
     return url_lists
 
 def download_videos(url_lists) :
@@ -39,14 +37,34 @@ def download_videos(url_lists) :
         best = video.getbest(preftype="mp4")
         filename = best.download(quiet=False)
 
-def channels_list_by_channel_playlist(service):
-    get_playlist_items = service.playlistItems().list(part='snippet,contentDetails',
-    maxResults=10,
-    playlistId='your_playlist_ID').execute()
+def collect_urls(get_playlist_items):
+    local_lists = []
+    for items in get_playlist_items['items']:
+        local_lists.append(items['snippet']['resourceId']['videoId'])
 
+    return local_lists
+
+def channels_list_by_channel_playlist(service):
+    #Get the upload_id first
+    get_all_videos = service.channels().list(part='snippet,contentDetails',
+                                           id='channel_id').execute()
+    upload_id = get_all_videos['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    #Get all the videos uploaded by that id
     url_lists = []
-    for items in get_playlist_items['items'] :
-        url_lists.append(items['snippet']['resourceId']['videoId'])
+    get_playlist_items = service.playlistItems().list(part='snippet,contentDetails',
+                                                     maxResults=50,
+                                                     playlistId=upload_id).execute()
+
+    url_lists.extend(collect_urls(get_playlist_items))
+    #To get results from the next page get the next page token from each request
+    while('nextPageToken' in get_playlist_items.keys()) :
+        get_playlist_items = service.playlistItems().list(part='snippet,contentDetails',
+                                                        maxResults=50,
+                                                        playlistId=upload_id,
+                                                        pageToken = get_playlist_items['nextPageToken']).execute()
+
+        url_lists.extend(collect_urls(get_playlist_items))
 
     return  url_lists
 
@@ -54,4 +72,5 @@ if __name__ == '__main__' :
     service = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                     developerKey=DEVELOPER_KEY, cache_discovery=False)
     url_lists = channels_list_by_channel_playlist(service)
+    pdb.set_trace()
     download_videos(url_lists)
